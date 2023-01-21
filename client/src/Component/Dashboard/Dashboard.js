d037377f4e81b422eb806b504281df2a79f3edfb
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2023-01-21 11:32:11 +0530

    html completed

--- client/src/Component/Dashboard/Dashboard.js
+++ client/src/Component/Dashboard/Dashboard.js
@@ -3,6 +3,38 @@ const Dashboard = () => {
     return(
         <div>
             <h1>Dashboard Page</h1>
+            <div className="main-container">
+
+               <div className="title-card">
+                  <div><h1>ToDo</h1></div>
+                  <div>
+                    <h3>Today</h3>
+                    <p>21 jan 2023</p>
+                    <p>picture</p>
+                  </div>
+               </div>
+               <hr/>
+
+               <div className="input-container">
+                 <div>
+                    <input placeholder="Add a new task" type="text"></input>
+                    <button>+</button>
+                 </div>
+               </div>
+               <hr/>
+
+               <div className="task-container">
+                  <div className="bending">
+                    <input type="checkbox"></input>
+                    <h4>Buy Water</h4>
+                  </div>
+                  <hr/>
+                  <div className="completed">
+                    <input type="checkbox"></input>
+                    <h4>Buy headphone</h4>
+                  </div>
+               </div>
+            </div>
         </div>
     )
 }
