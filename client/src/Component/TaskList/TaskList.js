f0744fd3c9fab6f592a08352aa405c9b28e41fdc
Daniel-Charles-J <80055586+Daniel-Charles-J@users.noreply.github.com>
2023-01-21 16:57:23 +0530

    TaskList added

--- client/src/Component/TaskList/TaskList.js
+++ client/src/Component/TaskList/TaskList.js
@@ -1,54 +1,50 @@
 import{BiEdit} from "react-icons/bi"
 import {RiDeleteBin6Line} from "react-icons/ri"
 import "../Dashboard/Dashboard.css"
+import "./TaskList.css"
 
 const TaskList = () =>{
+    let completed = ["Studying"];
+    let wishList = ["Doing Cycle", "Proper Diet",]
     return(
         <div>
             <div className="task-container">
                   <div className="all-task">
 
-                  <div className="task-container-2">
-                        <div className="completed tasks">
-                          <input type="checkbox"></input>
-                          <h4>Buy headphone</h4>
-                        </div>
-                        <div className="icons">
-                          <BiEdit />
-                          <RiDeleteBin6Line/>  
-                       </div>              
-                    </div>
-
+                  {wishList.map((x) => {
+                        return(
+                            <div className="task-container-2">
+                              <div className="tasks">
+                                <input type="checkbox"></input>
+                                <h4>{x}</h4>
+                            </div>
+                            <div className="icons">
+                              <BiEdit />
+                              <RiDeleteBin6Line/>  
+                           </div>              
+                            </div>
+                        )
+                    })}
                   </div>
 
                   <hr/>
 
                   <div className="all-task">
 
-                  <div className="task-container-2">
-                        <div className="completed tasks">
-                          <input type="checkbox"></input>
-                          <h4>Buy headphone</h4>
-                        </div>
-                        <div className="icons">
-                          <BiEdit />
-                          <RiDeleteBin6Line/>  
-                       </div>              
-                    </div>
-
-                   
-                    
-                    <div className="task-container-2">
-                        <div className="completed tasks">
-                          <input type="checkbox"></input>
-                          <h4>Buy headphone</h4>
-                        </div>
-                        <div className="icons">
-                          <BiEdit />
-                          <RiDeleteBin6Line/>  
-                       </div>              
-                    </div>
-
+                    {completed.map((x) => {
+                        return(
+                            <div className="task-container-2">
+                              <div className="tasks">
+                                <input type="checkbox"></input>
+                                <h4>{x}</h4>
+                            </div>
+                            <div className="icons">
+                              <BiEdit />
+                              <RiDeleteBin6Line/>  
+                           </div>              
+                            </div>
+                        )
+                    })}
                   </div>
                </div>
         </div>
